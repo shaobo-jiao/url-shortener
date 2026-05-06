aeaa7dc7131295c5e91e36aea18a9c484939777a
Shaobo Jiao <shaobojiao@outlook.com>
2026-05-06 12:29:23 +0800

    added Base62 Encoding impl and tests

---
+++ UrlShortener.Api/Common/Base62Encoding.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace UrlShortener.Api;
+
+/// <summary>
+/// Base62 encoding that converts a long integer to its Base62 text representation and vice versa
+/// </summary>
+public static class Base62Encoding
+{
+    public const string Charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    public static string Encode(long val)
+    {
+        if (val < 0) throw new ArgumentOutOfRangeException(nameof(val), val, "Non-negative value expected");
+        if (val == 0) return Charset[..1];
+
+        // algo: e.g. 11111 => 179*62 + 13 = (2*62 + 55) * 62 + 13 => 2*62^2 + 55*62 + 13
+        var text = new char[11]; // max long integer: 9,223,372,036,854,775,807 => AzL8n0Y58m7
+        int index = 10;
+
+        while (val > 0)
+        {
+            long quotient = val / 62;
+            int reminder = (int)(val - quotient * 62);
+            text[index--] = Charset[reminder];
+            val = quotient;
+        }
+        return new string(text, index + 1, text.Length - index - 1); // encoded text starts from current index + 1;
+    }
+
+    public static long Decode(string text)
+    {
+        if (string.IsNullOrEmpty(text) || text.Length > 11) // quick check on obvious invalid input 
+            throw new ArgumentOutOfRangeException(nameof(text), text);
+
+        long val = 0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            int cv = Charset.IndexOf(text[i]);
+            if (cv == -1) throw new InvalidOperationException("Invalid character in Base62 Encoding");
+
+            // check overflow, whether val*62 + cv > long.MaxValue <==> whether val > (long.MaxValue - cv) / 62
+            if (val > (long.MaxValue - cv) / 62)
+                throw new ArgumentOutOfRangeException(nameof(text), text);
+
+            val = val * 62 + cv;
+        }
+        return val;
+    }
+}
---
+++ UrlShortener.Tests/Base62EncodingTests.cs
@@ -0,0 +1,64 @@
+﻿using UrlShortener.Api;
+
+namespace UrlShortener.Tests;
+
+public class Base62EncodingTests
+{
+    [Theory]
+    [InlineData(0, "0")]
+    [InlineData(1, "1")]
+    [InlineData(61, "z")]
+    [InlineData(62, "10")]
+    [InlineData(1234567890, "1LY7VK")]
+    [InlineData(long.MaxValue, "AzL8n0Y58m7")]
+    public void Encode_ValidInput_ReturnsCorrectEncodedText(long value, string expected)
+    {
+        string encoded = Base62Encoding.Encode(value);
+
+        Assert.Equal(expected, encoded);
+    }
+
+    [Fact]
+    public void Encode_NegativeInput_ThrowsArgumentOutOfRangeException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => Base62Encoding.Encode(-1));
+    }
+
+    [Theory]
+    [InlineData("0", 0L)]
+    [InlineData("1", 1L)]
+    [InlineData("z", 61L)]
+    [InlineData("10", 62L)]
+    [InlineData("1LY7VK", 1234567890L)]
+    [InlineData("AzL8n0Y58m7", long.MaxValue)]
+    public void Decode_ValidInput_ReturnsCorrectDecodedValue(string text, long expected)
+    {
+        long value = Base62Encoding.Decode(text);
+
+        Assert.Equal(expected, value);
+    }
+
+    [Fact]
+    public void Decode_EmptyInput_ThrowsArgumentOutOfRangeException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => Base62Encoding.Decode(string.Empty));
+    }
+
+    [Fact]
+    public void Decode_TooLongInput_ThrowsArgumentOutOfRangeException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => Base62Encoding.Decode("0123456789AB"));
+    }
+
+    [Fact]
+    public void Decode_InputContainsInvalidChars_ThrowsInvalidOperationException()
+    {
+        Assert.Throws<InvalidOperationException>(() => Base62Encoding.Decode("0*0"));
+    }
+
+    [Fact]
+    public void Decode_InputExceedingMaxValue_ThrowsArgumentOutOfRangeException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => Base62Encoding.Decode("AzL8n0Y58m8"));
+    }
+}
